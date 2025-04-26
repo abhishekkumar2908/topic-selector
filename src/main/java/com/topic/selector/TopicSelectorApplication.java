cb52105670676efbc23845735efd2e3547a8a6ce
abhishekkumar2908 <abhishekkumar29081999@gmail.com>
2025-04-26 18:33:20 +0530

    This is a topic seletor service

---
+++ src/main/java/com/topic/selector/TopicSelectorApplication.java
@@ -0,0 +1,13 @@
+package com.topic.selector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicSelectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicSelectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topic/selector/TopicSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topic.selector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicSelectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
