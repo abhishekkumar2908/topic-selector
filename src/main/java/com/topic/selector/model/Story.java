7a4b12cf08e66018d645f46300102ad66761026c
abhishekkumar2908 <abhishekkumar29081999@gmail.com>
2025-04-26 21:55:54 +0530

    fix: lombok

--- src/main/java/com/topic/selector/model/Story.java
+++ src/main/java/com/topic/selector/model/Story.java
@@ -1,7 +1,10 @@
 package com.topic.selector.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
 
+
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Story {
 
@@ -9,36 +12,4 @@ public class Story {
     private String url;
     private String by;
 
-    public Story(String title, String url, String by) {
-        this.title = title;
-        this.url = url;
-        this.by = by;
-    }
-
-    public Story() {
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getBy() {
-        return by;
-    }
-
-    public void setBy(String by) {
-        this.by = by;
-    }
 }
